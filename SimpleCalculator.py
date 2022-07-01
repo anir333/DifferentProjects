51756d1abd93065c86fc806089072c9555153030
Anir <anirtraning@gmail.com>
2022-07-01 23:54:50 +0200

    Initial commit with SimpleCalculator.py file.

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -9,22 +9,26 @@ print("""Choose what operation do you want to do:
 while True:
     answer_option = input("Answer: ")
 
-    if answer_option.lower() == "add":
+        if answer_option.lower() == "add"  or answer_option.lower() == "+":
         x = int(input("First number: "))
         y = int(input("Second number: "))
         print(f"{x} + {y} = ", x + y)
-    elif answer_option.lower() == "subtract":
+        break
+    elif answer_option.lower() == "subtract" or answer_option.lower() == "-":
         b = int(input("First number: "))
         c = int(input("Second number: "))
         print(f"{b} - {c} = ", b - c)
-    elif answer_option.lower() == "multiply":
+        break
+    elif answer_option.lower() == "multiply" or answer_option.lower() == "*":
         e = int(input("First number: "))
         o = int(input("Second number: "))
         print(f"{e} * {o} = ", e * o)
-    elif answer_option.lower() == "divide":
+        break
+    elif answer_option.lower() == "divide" or answer_option.lower() == "/":
         p = int(input("First number: "))
         t = int(input("Second number: "))
         print(f"{p} / {t} = ", p // t)
+        break
     elif answer_option.lower() == "stop":
         break
     else:
